20b2333a7307159066f138158a61a963de07e841
guzl1k <124172337+guzleek@users.noreply.github.com>
2023-06-05 17:54:46 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const btnDarkMode = document.querySelector(".dark-mode-btn");
+
+//Проверка темной темы в localstorage
+if (localStorage.getItem('darkMode') === 'dark') {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add("dark");
+} else if (localStorage.getItem("darkMode") === "light") {
+    btnDarkMode.classList.remove("dark-mode-btn--active");
+    document.body.classList.remove("dark");
+}
+
+
+
+//Включение ночного режима по кнопке 
+btnDarkMode.onclick = function () {
+    btnDarkMode.classList.toggle("dark-mode-btn--active");
+    const isDark = document.body.classList.toggle("dark");
+
+    if (isDark) {
+        localStorage.setItem("darkMode", "dark");
+    } else {
+        localStorage.setItem("darkMode", "light");
+    }
+};
+
